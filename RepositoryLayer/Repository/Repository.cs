11112fcf26d81e85af5b98ea61475fed5c1c7a6a
Ricardo <delacruzmartinezricardoe@gmail.com>
2022-08-02 12:19:45 -0400

    RepositoryLayerFirstModification

---
+++ RepositoryLayer/Repository/Repository.cs
@@ -0,0 +1,67 @@
+﻿using DomainLayer.Data;
+using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.IRepository;
+
+namespace RepositoryLayer.Repository
+{
+    public class Repository <T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly DataContext _context;
+        private DbSet<T> entities;
+
+        public Repository(DataContext dataContext)
+        {
+            _context = dataContext;
+            entities = _context.Set<T>();
+        }
+
+        public void Delete(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Remove(entity);
+            _context.SaveChanges();
+        }
+        public T Get(int Id)
+        {
+            return entities.SingleOrDefault(c => c.Id == Id);
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return entities.AsEnumerable();
+        }
+        public void Insert(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Add(entity);
+            _context.SaveChanges();
+        }
+        public void Remove(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Remove(entity);
+        }
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+        public void Update(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException("entity");
+            }
+            entities.Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
